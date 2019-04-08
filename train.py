37452df660c0ea694525079cf8c40c819810ccd3
tadax <tadax@xtn.jp>
2019-04-09 00:24:54 +0900

    Change default batch size

--- train.py
+++ train.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     parser.add_argument('--label_dir', required=True)
     parser.add_argument('--model_dir', default='weights/')
     parser.add_argument('--log_path', default='weights/out.log')
-    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--learning_rate', type=float, default=1e-4)
     parser.add_argument('--scale', type=float, default=20.0)
     parser.add_argument('--num_classes', type=int, default=80)
